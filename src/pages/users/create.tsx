f07dfd7ea7b9f57d92a549330f1a99e6cbf952ae
AndreSDS <andre_smiths@outlook.com>
2023-01-10 19:42:48 -0300

    usando useMutation para criar um usuário

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -8,13 +8,17 @@ import {
   SimpleGrid,
   VStack,
 } from "@chakra-ui/react";
-import {yupResolver} from "@hookform/resolvers/yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 import Link from "next/link";
-import {SubmitHandler, useForm} from "react-hook-form";
+import { useRouter } from "next/router";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { useMutation } from "react-query";
 import * as yup from "yup";
-import {Input} from "../../components/Form/Input";
-import {Header} from "../../components/Header";
-import {SideBar} from "../../components/SideBar";
+import { Input } from "../../components/Form/Input";
+import { Header } from "../../components/Header";
+import { SideBar } from "../../components/SideBar";
+import { api } from "../../services/api";
+import { queryClient } from "../../services/query/queryClient";
 
 interface CreateUserFormData {
   nome: string;
@@ -36,19 +40,37 @@ const createUserFormSchema = yup.object().shape({
 });
 
 export default function CreateUser() {
+  const router = useRouter();
+  const { mutateAsync } = useMutation(async (user: CreateUserFormData) => {
+    const response = await api.post("users", {
+      user: {
+        ...user,
+        created_at: new Date(),
+      },
+    });
+
+    return response.data.user;
+  },{
+    onSuccess: () => {
+      queryClient.invalidateQueries("users");
+    }
+  });
+
   const {
     register,
     handleSubmit,
     formState,
-    formState: {errors},
+    formState: { errors },
   } = useForm({
     resolver: yupResolver(createUserFormSchema),
   });
 
-  const handleCreateUser: SubmitHandler<CreateUserFormData> = async (data) => {
-    await new Promise((resolve) => setTimeout(resolve, 2000));
+  const handleCreateUser: SubmitHandler<CreateUserFormData> = async (
+    values
+  ) => {
+    await mutateAsync(values);
 
-    console.log({data});
+    router.push('/users'); 
   };
 
   return (

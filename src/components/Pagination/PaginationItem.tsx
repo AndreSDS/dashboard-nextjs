9a8de1a24c9e176d9ac1a1fc09863f424280baa7
AndreSDS <andre_smiths@outlook.com>
2023-01-10 15:17:33 -0300

    implementando lógica de paginação

--- src/components/Pagination/PaginationItem.tsx
+++ src/components/Pagination/PaginationItem.tsx
@@ -1,13 +1,15 @@
-import {Button} from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
 
 interface PaginationItemProps {
-  isCurrent?: boolean;
   pageNumber: number;
+  setCurrentPage?: (page: number) => void;
+  isCurrent?: boolean;
 }
 
 export function PaginationItem({
   isCurrent = false,
   pageNumber,
+  setCurrentPage,
 }: PaginationItemProps) {
   if (isCurrent) {
     return (
@@ -36,6 +38,7 @@ export function PaginationItem({
       _hover={{
         bgColor: "gray.500",
       }}
+      onClick={() => setCurrentPage(pageNumber)}
     >
       {pageNumber}
     </Button>
--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,7 +1,42 @@
-import {Box, Stack} from "@chakra-ui/react";
-import {PaginationItem} from "./PaginationItem";
+import { Box, Stack, Text } from "@chakra-ui/react";
+import { PaginationItem } from "./PaginationItem";
 
-export function Pagination() {
+interface PaginationProps {
+  totalCountOfRegisters: number;
+  currentPage: number;
+  registersPerPage?: number;
+  setCurrentPage: (page: number) => void;
+}
+
+const siblingsCount = 1;
+
+function generatePagesArray(from: number, to: number) {
+  return [...new Array(to - from)]
+    .map((_, index) => from + index + 1)
+    .filter((page) => page > 0);
+}
+
+export function Pagination({
+  totalCountOfRegisters,
+  currentPage = 1,
+  registersPerPage = 10,
+  setCurrentPage,
+}: PaginationProps) {
+  const lastPage = Math.floor(totalCountOfRegisters / registersPerPage);
+
+  const previousPage =
+    currentPage > 1
+      ? generatePagesArray(currentPage - 1 - siblingsCount, currentPage - 1)
+      : [];
+
+  const nextPage =
+    currentPage < lastPage
+      ? generatePagesArray(
+          currentPage,
+          Math.min(currentPage + siblingsCount, lastPage)
+        )
+      : [];
+
   return (
     <Stack
       direction={["column", "row"]}
@@ -11,14 +46,44 @@ export function Pagination() {
       align="center"
     >
       <Box>
-        <strong>0</strong> - <strong>10</strong> de <strong>100</strong>
+        <strong>{currentPage}</strong> - <strong>{registersPerPage}</strong>{" "}
+        de <strong>{totalCountOfRegisters}</strong>
       </Box>
 
       <Stack direction="row" spacing="2">
-        <PaginationItem pageNumber={1} isCurrent />
-        <PaginationItem pageNumber={2} />
-        <PaginationItem pageNumber={3} />
-        <PaginationItem pageNumber={4} />
+        {currentPage > 1 + siblingsCount ? (
+          <PaginationItem setCurrentPage={setCurrentPage} pageNumber={1} />
+        ) : null}
+
+        {currentPage > 2 + siblingsCount ? (
+          <Text color="gray.300" width="8" textAlign="center">
+            ...
+          </Text>
+        ) : null}
+
+        {previousPage.length > 0
+          ? previousPage.map((page) => (
+              <PaginationItem setCurrentPage={setCurrentPage} key={page} pageNumber={page} />
+            ))
+          : null}
+
+        <PaginationItem pageNumber={currentPage} isCurrent />
+
+        {nextPage.length > 0
+          ? nextPage.map((page) => (
+              <PaginationItem setCurrentPage={setCurrentPage} key={page} pageNumber={page} />
+            ))
+          : null}
+
+        {currentPage + 1 + siblingsCount < lastPage ? (
+          <Text color="gray.300" width="8" textAlign="center">
+            ...
+          </Text>
+        ) : null}
+
+        {currentPage + siblingsCount < lastPage ? (
+          <PaginationItem setCurrentPage={setCurrentPage} pageNumber={lastPage} />
+        ) : null}
       </Stack>
     </Stack>
   );

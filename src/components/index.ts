f73264dba486e77fb4848b182e00f00e67332ccf
AndreSDS <andre_smiths@outlook.com>
2023-01-10 15:15:50 -0300

    criando alias para pasta components

---
+++ src/components/index.ts
@@ -0,0 +1,12 @@
+import { Header } from './Header';
+import { Input } from './Form/Input';
+import { SideBar } from './SideBar';
+import { Pagination } from './Pagination';
+
+
+export {
+    Header,
+    Input,
+    SideBar,
+    Pagination,
+}
